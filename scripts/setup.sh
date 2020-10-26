01deecee8e19019d32f47189ac13325a1b080a68
Yoshiyuki Kono <yoshiyuki.kono@gmail.com>
2020-10-26 16:15:06 +0900

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,9 @@
+
+
+sudo yum install -y gcc gcc-c++ python3-devel python3-pip cmake
+sudo yum install -y openssl-devel
+sudo python3 -m pip install --upgrade pip setuptools wheel
+sudo python3 -m pip install couchbase
+
 sudo yum install -y git
-sudo yum install -y python3
+
